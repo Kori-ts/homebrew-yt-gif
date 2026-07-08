8367560b1eab9c51c953e3d186f35e50d00dc133
Kori-ts <278865040+Kori-ts@users.noreply.github.com>
2026-07-09 00:29:37 +0200

    chore: bump yt-gif to 1.0.1

--- Formula/yt-gif.rb
+++ Formula/yt-gif.rb
@@ -1,30 +1,30 @@
 class YtGif < Formula
   desc "Simple CLI tool for creating GIFs from YouTube videos"
   homepage "https://github.com/Kori-ts/yt-gif"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.0/yt-gif_1.0.0_darwin_amd64.tar.gz"
-      sha256 "891f6cd6baacd2036918ce59b36e46aae5d081c1d4674258973179ca7e02eb88"
+      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.1/yt-gif_1.0.1_darwin_amd64.tar.gz"
+      sha256 "245bf51218c74b014a24ac82d89ea51316a6551b18d6c3feddc32156a343ecc3"
     end
 
     on_arm do
-      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.0/yt-gif_1.0.0_darwin_arm64.tar.gz"
-      sha256 "04421e591fe4f416a72ad0ec9215fc1af5bdd51a288b65ba0f509d9c5cc7be78"
+      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.1/yt-gif_1.0.1_darwin_arm64.tar.gz"
+      sha256 "fe05d248663cd3c21e6c77b59450468adbd643588e48e67279f3a40c766cf167"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.0/yt-gif_1.0.0_linux_amd64.tar.gz"
-      sha256 "50af4bfc67e284929f0dca1efa5e168798840d3e69c491377e86a8d581b86500"
+      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.1/yt-gif_1.0.1_linux_amd64.tar.gz"
+      sha256 "156278512884d2e2e2df9b3d30b3f5f0bf51a76f2e07efa170658a5a0a6fd675"
     end
 
     on_arm do
-      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.0/yt-gif_1.0.0_linux_arm64.tar.gz"
-      sha256 "7fffb758e44d4258e862071a0658bd3f547f4c594e51b275996f292dc0a783d6"
+      url "https://github.com/Kori-ts/yt-gif/releases/download/v1.0.1/yt-gif_1.0.1_linux_arm64.tar.gz"
+      sha256 "f8dc3853753fff9df815efadbefefc7c880162b8843ab7858ad023fb29b2fa21"
     end
   end
 
